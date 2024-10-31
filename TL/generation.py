b82ed548170b5f59aec2ffa531226351d23bb381
Mingxuan Zhang <zhan3692@purdue.edu>
2024-10-31 11:39:03 -0400

    dd

--- TL/generation.py
+++ TL/generation.py
@@ -1,6 +1,7 @@
 import torch
 import math
 import lightning as L
+from typing import Union
 
 class Tempering(L.LightningModule):
     def __init__(
@@ -11,7 +12,7 @@ class Tempering(L.LightningModule):
         T: int,
         num_mc_steps: int,      
         zeta: float = 1.0,
-        mc_subset_ratio: float = 0.5,
+        mc_subset_ratio: Union[float, int] = 0.5,
         burn_in_fraction: float = 0.5,
         random_walk_step_size: float = 0.0,
         prediction_type: str = "epsilon"
@@ -25,37 +26,40 @@ class Tempering(L.LightningModule):
         assert 1 <= T <= self.noise_scheduler.config.num_train_timesteps, "T must be greater or equal to 1 and less or equal to the number of training timesteps of the noise scheduler"
         self.T = T
         self.noise_scheduler.set_timesteps(T)
+        self.timesteps = torch.flip(self.noise_scheduler.timesteps, [0]).to(self.device)
 
+        # calculate the cumulative product of reversed alphas
         self.alphas_reversed_cumprod = torch.cumprod(torch.flip(self.noise_scheduler.alphas, [0]), dim=0)
         # each prod starts from the current timestep to the end
         self.alphas_reversed_cumprod = torch.flip(self.alphas_reversed_cumprod, [0])
 
+        # set zeta
         assert zeta > 0, "zeta must be greater than 0"
         self.zeta = zeta
 
-        # set MC_steps
+        # set the number of Monte Carlo steps
         assert num_mc_steps >= 1, "num_mc_steps must be greater or equal to 1"
         self.num_mc_steps = num_mc_steps    
 
-        # set burn in fraction
+        # set the number of burn-in steps
         assert 0 <= burn_in_fraction < 1, "burn_in_fraction must be greater or equal to 0 and less than 1"
         self.burn_in_fraction = burn_in_fraction
         self.burn_in_steps = int(burn_in_fraction * self.num_mc_steps)
 
-        # calculate the number of MC samples
+        # calculate the number of Monte Carlo samples
         self.num_mc_samples = self.num_mc_steps - self.burn_in_steps
         assert self.num_mc_samples > 0, "n_MC must be greater than 0, please adjust burn_in_fraction or num_mc_steps accordingly!"
 
-        # set mc_subset_size
+        # set the size of the Monte Carlo subset that we randomly select from the Monte Carlo sample buffers
         self.mc_subset_size = int(mc_subset_ratio * self.num_mc_samples) if 0 < mc_subset_ratio < 1 and type(mc_subset_ratio) == float else mc_subset_ratio
         assert 0 < self.mc_subset_size <= self.num_mc_samples, "mc_subset_size must be greater than 0 and less or equal to self.num_mc_samples, please adjust mc_subset_ratio accordingly!"
 
-        # random walk step size
+        # set therandom walk step size
         assert random_walk_step_size >= 0, "random_walk_step_size must be greater or equal to 0"
         self.random_walk_step_size = random_walk_step_size
 
+        assert prediction_type in ["epsilon", "sample"], "prediction_type must be either 'epsilon' or 'sample'"
         self.prediction_type = prediction_type
-        assert self.prediction_type in ["epsilon", "sample"], "prediction_type must be either 'epsilon' or 'sample'"
 
         # set the number of training samples
         self.num_training_samples = num_training_samples
@@ -97,23 +101,26 @@ class Tempering(L.LightningModule):
         clean_images = batch["images"]
 
         # add noise to the clean images
+        # if t == self.T-1, the noise is not added
         if t == self.T-1:
             noisy_images = clean_images  
+        # if t < self.T-1, the noise is added
         elif t < self.T-1:
-            timestep = self.noise_scheduler.timesteps[self.T - t - 2].to(clean_images.device)
+            timestep = self.timesteps[self.T - t - 2].to(clean_images.device)
             noisy_images = self.noise_scheduler.add_noise(clean_images, torch.randn_like(clean_images), timestep)
+        # if t is not in the range of 0 to self.T-1, raise an error
         else:
             raise ValueError(f"Invalid timestep: {t}, please choose from 0 to {self.T-1}")
 
         # draw epsilon from the standard normal distribution
         epsilon = torch.rand_like(clean_images)
 
-        # add speilon to the noisy images to generate pesudo noise from there
-        timestep = self.noise_scheduler.timesteps[self.T - t - 1].to(clean_images.device)
-        pesudo_noise = self.add_noise_from_timestep(noisy_images, epsilon, timestep)
+        # add epsilon to the noisy images to generate pesudo white (standard normal) noise from there
+        timestep = self.timesteps[self.T - t - 1].to(clean_images.device)
+        pesudo_white_noise = self.add_noise_from_timestep(noisy_images, epsilon, timestep)
 
         # predict the epsilon
-        predicted_epsilon = self.model(pesudo_noise, t).sample
+        predicted_epsilon = self.model(pesudo_white_noise, t).sample
 
         # calculate the mean squared error loss
         mse_loss = torch.nn.functional.mse_loss(epsilon, predicted_epsilon)/2
@@ -129,14 +136,14 @@ class Tempering(L.LightningModule):
         if t >= self.T-1:
             noisy_images = clean_images
         else:   
-            timestep = self.noise_scheduler.timesteps[self.T - t - 2].to(clean_images.device)
+            timestep = self.timesteps[self.T - t - 2].to(clean_images.device)
             noisy_images = self.noise_scheduler.add_noise(clean_images, torch.randn_like(clean_images), timestep)
 
         # draw epsilon from the standard normal distribution
         epsilon = torch.rand_like(clean_images)
 
         # add epsilon to the noisy images to generate pesudo noise from there
-        timestep = self.noise_scheduler.timesteps[self.T - t - 1].to(clean_images.device)
+        timestep = self.timesteps[self.T - t - 1].to(clean_images.device)
         pesudo_noise = self.add_noise_from_timestep(noisy_images, epsilon, timestep)
 
         # predict the noisy images
@@ -212,7 +219,11 @@ class Tempering(L.LightningModule):
             device=self.device
         )
 
+        assert t is None or 0 <= t < self.T, "t must be in the range of 0 to self.T-1"
+
         t = self.T-1 if t is None else t
+
+        timestep = -1 if t == self.T-1 else self.timesteps[t].to(noise.device)
 
         if self.prediction_type == "epsilon":
 
