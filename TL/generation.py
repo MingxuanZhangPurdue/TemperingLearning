cf32f64b33aa690d41e272b0d3350e11decbc7f0
Mingxuan Zhang <zhan3692@purdue.edu>
2024-10-31 15:46:07 -0400

    ggg

--- TL/generation.py
+++ TL/generation.py
@@ -13,9 +13,8 @@ class Tempering(L.LightningModule):
         num_mc_steps: int,      
         zeta: float = 1.0,
         mc_subset_ratio: Union[float, int] = 0.5,
-        burn_in_fraction: float = 0.5,
+        burn_in_fraction: float = 0.7,
         random_walk_step_size: float = 0.0,
-        prediction_type: str = "epsilon"
     ):
         super().__init__()
         
@@ -25,8 +24,7 @@ class Tempering(L.LightningModule):
 
         assert 1 <= T <= self.noise_scheduler.config.num_train_timesteps, "T must be greater or equal to 1 and less or equal to the number of training timesteps of the noise scheduler"
         self.T = T
-        self.noise_scheduler.set_timesteps(T)
-        self.timesteps = torch.flip(self.noise_scheduler.timesteps, [0]).to(self.device)
+        self.timestep_schedule = torch.linspace(0, self.noise_scheduler.config.num_train_timesteps - 1, T, dtype=torch.long)
 
         # calculate the cumulative product of reversed alphas
         self.alphas_reversed_cumprod = torch.cumprod(torch.flip(self.noise_scheduler.alphas, [0]), dim=0)
@@ -57,9 +55,6 @@ class Tempering(L.LightningModule):
         # set therandom walk step size
         assert random_walk_step_size >= 0, "random_walk_step_size must be greater or equal to 0"
         self.random_walk_step_size = random_walk_step_size
-
-        assert prediction_type in ["epsilon", "sample"], "prediction_type must be either 'epsilon' or 'sample'"
-        self.prediction_type = prediction_type
 
         # set the number of training samples
         self.num_training_samples = num_training_samples
@@ -97,26 +92,26 @@ class Tempering(L.LightningModule):
 
     def training_step_epsilon_prediction(self, batch, t):
 
+        assert 0 <= t <= self.T-1, "t must be in the range of 0 to self.T-1, including the boundary values!"
+
         # get the clean images
         clean_images = batch["images"]
 
-        # add noise to the clean images
-        # if t == self.T-1, the noise is not added
+        # add noise to the clean images to generate noisy images
         if t == self.T-1:
             noisy_images = clean_images  
-        # if t < self.T-1, the noise is added
-        elif t < self.T-1:
-            timestep = self.timesteps[self.T - t - 2].to(clean_images.device)
-            noisy_images = self.noise_scheduler.add_noise(clean_images, torch.randn_like(clean_images), timestep)
-        # if t is not in the range of 0 to self.T-1, raise an error
         else:
-            raise ValueError(f"Invalid timestep: {t}, please choose from 0 to {self.T-1}")
+            timestep = self.timestep_schedule[self.T - t - 2].to(clean_images.device)
+            noisy_images = self.noise_scheduler.add_noise(clean_images, torch.randn_like(clean_images), timestep)
 
         # draw epsilon from the standard normal distribution
-        epsilon = torch.rand_like(clean_images)
+        epsilon = torch.randn_like(clean_images)
 
         # add epsilon to the noisy images to generate pesudo white (standard normal) noise from there
-        timestep = self.timesteps[self.T - t - 1].to(clean_images.device)
+        if t == self.T-1:
+            timestep = self.timestep_schedule[0].to(clean_images.device)
+        else:
+            timestep = self.timestep_schedule[self.T - t - 2].to(clean_images.device)
         pesudo_white_noise = self.add_noise_from_timestep(noisy_images, epsilon, timestep)
 
         # predict the epsilon
@@ -126,42 +121,9 @@ class Tempering(L.LightningModule):
         mse_loss = torch.nn.functional.mse_loss(epsilon, predicted_epsilon)/2
         
         return mse_loss
-    
-    def training_step_sample_prediction(self, batch, t):
-
-        # get the clean images  
-        clean_images = batch["images"]
-
-        # add noise to the clean images
-        if t >= self.T-1:
-            noisy_images = clean_images
-        else:   
-            timestep = self.timesteps[self.T - t - 2].to(clean_images.device)
-            noisy_images = self.noise_scheduler.add_noise(clean_images, torch.randn_like(clean_images), timestep)
-
-        # draw epsilon from the standard normal distribution
-        epsilon = torch.rand_like(clean_images)
-
-        # add epsilon to the noisy images to generate pesudo noise from there
-        timestep = self.timesteps[self.T - t - 1].to(clean_images.device)
-        pesudo_noise = self.add_noise_from_timestep(noisy_images, epsilon, timestep)
-
-        # predict the noisy images
-        predicted_noisy_images = self.model(pesudo_noise, t).sample
-
-        mse_loss = torch.nn.functional.mse_loss(noisy_images, predicted_noisy_images)/2
-
-        return mse_loss
-
 
     def training_step(self, batch, t):
-        # choose the prediction type
-        if self.prediction_type == "epsilon":
-            return self.training_step_epsilon_prediction(batch, t)
-        elif self.prediction_type == "sample":
-            return self.training_step_sample_prediction(batch, t)
-        else:
-            raise ValueError(f"Invalid prediction type: {self.prediction_type}, please choose from ['epsilon', 'sample']")
+        return self.training_step_epsilon_prediction(batch, t)
     
 
     def on_before_optimizer_step(self, t):
@@ -213,37 +175,33 @@ class Tempering(L.LightningModule):
 
 
     def generate_samples(self, batch_size, t=None):
-        # Generate initial noise
-        noise = torch.randn(
-            (batch_size, self.model.config.in_channels, self.model.config.sample_size, self.model.config.sample_size),
-            device=self.device
-        )
 
-        assert t is None or 0 <= t < self.T, "t must be in the range of 0 to self.T-1"
+        assert t is None or 0 <= t <= self.T - 1, "t must be in the range of 0 to self.T-1"
 
         t = self.T-1 if t is None else t
 
-        timestep = -1 if t == self.T-1 else self.timesteps[t].to(noise.device)
+        # Generate initial noise
+        white_noise = torch.randn(
+            (batch_size, self.model.config.in_channels, self.model.config.sample_size, self.model.config.sample_size),
+            device=self.model.device
+        )
 
-        if self.prediction_type == "epsilon":
-
-            # Predict epsilon for the first timestep
-            predicted_epsilon = self.model(noise, t).sample
-
-            # Get the cumulative product of alphas for the last timestep
-            alpha_prod = self.noise_scheduler.alphas_cumprod[timestep].to(device=noise.device)
-
-            # Calculate beta_prod
-            beta_prod = 1 - alpha_prod
-
-            # Predict the original sample
-            pred_original_sample = (noise - beta_prod**0.5 * predicted_epsilon) / alpha_prod**0.5
-
-        elif self.prediction_type == "sample":
-
-            pred_original_sample = self.model(noise, t).sample
-
+        # get the timestep
+        if t == 0:
+            timestep = self.timestep_schedule[-2].to(white_noise.device)
         else:
-            raise ValueError(f"Invalid prediction type: {self.prediction_type}, please choose from ['epsilon', 'sample']")
+            timestep = self.timestep_schedule[self.T - t - 1].to(white_noise.device)
+
+        # Predict epsilon for the first timestep
+        predicted_epsilon = self.model(white_noise, t).sample
+
+        # Get the cumulative product of alphas for the last timestep
+        alpha_reversed_prod = self.alphas_reversed_cumprod[timestep].to(device=white_noise.device)
+
+        # Calculate beta_prod
+        beta_reversed_prod = 1 - alpha_reversed_prod
+
+        # Predict the original sample
+        pred_original_sample = (white_noise - beta_reversed_prod**0.5 * predicted_epsilon) / alpha_reversed_prod**0.5
 
         return pred_original_sample
