1498f104110a37486c7e7404585a6fc58f00879a
Mingxuan Zhang <zhan3692@purdue.edu>
2024-11-06 15:41:56 -0500

    update code repo

---
+++ TL/distributed.py
@@ -0,0 +1,19 @@
+import os
+from functools import lru_cache
+
+@lru_cache()
+def get_is_torch_run() -> bool:
+    return os.environ.get("LOCAL_RANK") is not None
+
+@lru_cache()
+def get_is_slurm_job() -> bool:
+    return "SLURM_JOB_ID" in os.environ and not get_is_torch_run()
+
+@lru_cache()
+def get_global_rank() -> int:
+    if get_is_torch_run():
+        return int(os.environ["RANK"])
+    elif get_is_slurm_job():
+        return int(os.environ["SLURM_PROCID"])
+    else:
+        return 0
--- TL/logger.py
+++ TL/logger.py
@@ -3,26 +3,7 @@ import logging
 import math
 import sys
 import time
-import os
-
-from functools import lru_cache
-
-@lru_cache()
-def get_is_torch_run() -> bool:
-    return os.environ.get("LOCAL_RANK") is not None
-
-@lru_cache()
-def get_is_slurm_job() -> bool:
-    return "SLURM_JOB_ID" in os.environ and not get_is_torch_run()
-
-@lru_cache()
-def get_global_rank() -> int:
-    if get_is_torch_run():
-        return int(os.environ["RANK"])
-    elif get_is_slurm_job():
-        return int(os.environ["SLURM_PROCID"])
-    else:
-        return 0
+from TL.distributed import get_global_rank, get_is_slurm_job
 
 class LogFormatter(logging.Formatter):
     """
